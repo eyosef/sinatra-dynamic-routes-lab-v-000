587c7c69b8298341613a8afbc059183a030858d3
Eden Mesfun <emesfun@gmail.com>
2018-03-21 19:24:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -34,8 +34,8 @@ class App < Sinatra::Base
     @num1 = params[:number1].to_i
     @num2 = params[:number2].to_i
 
-    @add = @num1 + @num2
-    "#{@add}"
+    @result = @num1 + @num2
+    "#{result}"
   end
 
 end
