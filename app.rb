e62a00b12a2a461b54ac1257d567a8d10e2de6b4
Eden Mesfun <emesfun@gmail.com>
2018-03-21 19:22:15 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -32,7 +32,8 @@ class App < Sinatra::Base
 
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
-    
+    @num2 = params[:number2].to_i
+
     @add = params[:number1].to_i + params[:number2].to_i
     @add
   end
